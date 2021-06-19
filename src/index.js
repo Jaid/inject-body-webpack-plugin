a357399bd607a64806b558723cb879cf11ff2a7c
Jaid <jaid.jsx@gmail.com>
2021-06-19 11:46:31 +0200

    improve: Added debug log
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import HtmlWebpackPlugin from "html-webpack-plugin"
 import insertStringAfter from "insert-string-after"
 import insertStringBefore from "insert-string-before"
 
+import debug from "lib/debug"
+
 /**
  * @typedef {Object} Options
  * @prop {string} content
@@ -31,13 +33,16 @@ export default class InjectBrowserSyncPlugin {
    * @param {import("webpack").Compiler} compiler
    */
   apply(compiler) {
+    debug("Options: %o", this.options)
     compiler.hooks.compilation.tap(process.env.REPLACE_PKG_NAME, compilation => {
       HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(process.env.REPLACE_PKG_NAME, (data, cb) => {
+        debug("Before: %s", data.html)
         if (this.options.position === "end") {
           data.html = insertStringBefore(data.html, "</body>", this.options.content)
         } else {
           data.html = insertStringAfter(data.html, "<body>", this.options.content)
         }
+        debug(" After: %s", data.html)
         cb(null, data)
       })
     })
---
+++ src/lib/debug.js
@@ -0,0 +1,3 @@
+import debug from "debug"
+
+export default debug(process.env.REPLACE_PKG_NAME)
